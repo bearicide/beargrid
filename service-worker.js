93a42b345464ed5d65281e272c520884693cdab3
MATTBEAR <matt.baier86@gmail.com>
2026-05-23 13:27:14 -0400

    Bump BearGrid cache for machine modules

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,26 @@
-const CACHE='beargrid-v1.0.0';
+const CACHE='beargrid-v1.1.0-machine-modules';
 const ASSETS=[
   './',
   './index.html',
   './manifest.json',
   './assets/css/beargrid-machines.css',
-  './assets/js/machine.js'
+  './assets/js/machine.js',
+  './machines/drum-machine.html',
+  './machines/kaossilator-pro.html',
+  './machines/op-1.html',
+  './machines/orchid.html',
+  './machines/reese.html',
+  './machines/looping-drum-loops.html',
+  './machines/the-choppa.html',
+  './machines/sampla.html',
+  './machines/launcha.html',
+  './machines/mono-station.html',
+  './machines/mellotron.html',
+  './machines/bit-crusher.html',
+  './machines/fm-station.html',
+  './machines/delay-station.html',
+  './machines/filter-station.html',
+  './machines/master-fx.html'
 ];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
